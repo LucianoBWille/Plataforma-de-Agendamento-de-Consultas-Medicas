e043e2a1688aa0996e368213fd4974cfcd610663
LucianoBWille <00lucianobwille00@gmail.com>
2023-09-23 13:16:35 -0300

    feat(domain): Create Consultation model

---
+++ Domain/Domain/Models/Consultations/Consultation.cs
@@ -0,0 +1,25 @@
+namespace Domain;
+
+public class Consultation
+{
+    public long? ConsultationID { get; set; }
+
+    [Display(Name = "Paciente")]
+    public Patient? Patient { get; set; }
+
+    [Display(Name = "Médico")]
+    public Doctor? Doctor { get; set; }
+
+    [Display(Name = "Recepcionista")]
+    public Receptionist? Receptionist { get; set; }
+
+    [Display(Name = "Data e hora")]
+    public DateTime? dateTime { get; set; }
+
+    [Display(Name = "Observações")]
+    public string? Observations { get; set; }
+    
+    [RegularExpression("in-person|online")]
+    [Display(Name = "Tipo de Consulta")]
+    public string? ConsultationType { get; set; }
+}
