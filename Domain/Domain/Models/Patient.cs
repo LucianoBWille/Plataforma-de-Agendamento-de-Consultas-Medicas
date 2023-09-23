630814c662a5e6b96361b21a790ecc31b42ec319
LucianoBWille <00lucianobwille00@gmail.com>
2023-09-23 13:15:19 -0300

    feat(domain): Create Patient model

---
+++ Domain/Domain/Models/Patient.cs
@@ -0,0 +1,18 @@
+namespace Domain;
+
+public class Patient
+{
+    public long? PatientID { get; set; }
+
+    [Display(Name = "Nome")]
+    public string? FirstName { get; set; }
+
+    [Display(Name = "Sobrenome")]
+    public string? LastName { get; set; }
+
+    [Display(Name = "Número de identificação")]
+    public int? IdentificationNumber { get; set; }
+
+//     [Display(Name = "Histórico médico")]
+//     public List<Consutation>? MedicalHistory { get; set; }
+}
