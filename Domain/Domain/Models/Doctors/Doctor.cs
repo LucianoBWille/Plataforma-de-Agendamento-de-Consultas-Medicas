7ca92cae15e880b7084e357f237396d8a065ae60
LucianoBWille <00lucianobwille00@gmail.com>
2023-09-23 13:15:00 -0300

    feat(domain): Create Doctor model

---
+++ Domain/Domain/Models/Doctors/Doctor.cs
@@ -0,0 +1,22 @@
+namespace Domain;
+
+public class Doctor
+{
+    public long? DoctorID { get; set; }
+
+    [Display(Name = "Nome")]
+    public string? Name { get; set; }
+
+    [Display(Name = "Número de registro profissional")]
+    [Range(1, int.MaxValue, ErrorMessage = "Por favor insira um valor maior que 0")]
+    public int? ProfesssionalRegistrationNumber { get; set; }
+
+    [Display(Name = "Horários disponíveis")]
+    public List<DataTime>? AvailableHours { get; set; }
+
+    [Display(Name = "Especialidade")]
+    public Specialty? Specialty { get; set; }
+
+    // [Display(Name = "Consultas")]
+    // public List<Consultation>? Consultations { get; set; }
+}
